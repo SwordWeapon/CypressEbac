43090cfd473b3f262392c5b57233c30f27bee58f
SwordWeapon <matheuscarmofeliciano@gmail.com>
2025-01-28 16:49:26 -0300

    selecionar produtos usando page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,12 +1,30 @@
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe("Funcionalidade:produtos", () => {
   beforeEach(() => {
-    cy.visit("http://lojaebac.ebaconline.art.br/produtos/");
+    //cy.visit("http://lojaebac.ebaconline.art.br/produtos/");
+    produtosPage.visitarUrl()
   });
   it("Deve selecionar um produto da lista", () => {
     cy.get(" .product-block")
     //.eq(3)
     .contains('Arcadio Gym Short')
     .click()
-    cy.get('.woocommerce-product-details__short-description > p').should('contain', 'This is a variable product called a Arcadio Gym Short')
+    
+  });
+
+  it.only('Deve buscar um produto com sucesso', () => {
+    let produto = 'Ajax Full-Zip Sweatshirt'
+    produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+    cy.get('.product_title').should('contain', produto)
+  });
+
+  it('Deve visitar a página do produto', () => {
+    produtosPage.buscarProduto('')
+    
+  });
+
+  it('Deve adicionar o produto ao carrinho', () => {
+    
   });
 });
